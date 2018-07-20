b24f887039d21e12cae850d5fa3d48794e0e7d72
ferBaz <bazfer@gmail.com>
2018-07-20 18:59:00 -0500

    fast forward to before delete, now running phaser ce

--- src/states/Game.js
+++ src/states/Game.js
@@ -30,7 +30,7 @@ export default class extends Phaser.State {
 
   render() {
     if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+      // this.game.debug.spriteInfo(this.mushroom, 32, 32)
     }
   }
 }
